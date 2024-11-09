7ea46f9f6adf9a4339ad037245924c211f8bf41e
marcioandreribeiro31 <marcio.analista31@gmail.com>
2024-11-09 16:42:51 -0300

    Adicionado CSS do projeto

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,27 @@
 function checketExperience(){
     let heroName = document.getElementById("heroname").value;
     console.log(heroName);
-    let experience = document.getElementById("experience").value;
+    let experience = document.getElementById("experience").value;2
     console.log(experience);
         
-    if(heroName == ""  && experience == ""){
+    if(heroName == ""  || experience == ""){
         alert('Preencha os campos Nome e Experiência')
     }else if(experience < 1000){
-        document.getElementById("levelresult").innerText = `O herói é ${heroName} tem o XP de ${experience} Ferro`
+        document.getElementById("levelresult").innerText = `Resultado: O herói é ${heroName} tem o XP de ${experience} Ferro`
     }else if(experience >= 1001 && experience <= 2000){
-        document.getElementById("levelresult").innerText = `O herói ${heroName} tem o XP de ${experience} Bronze`
+        document.getElementById("levelresult").innerText = `Resultado: O herói ${heroName} tem o XP de ${experience} Bronze`
     }else if(experience >= 2001 && experience <= 5000){
-        document.getElementById("levelresult").innerText = `O herói ${heroName} tem o XP de ${experience} Prata`
+        document.getElementById("levelresult").innerText = `Resultado: O herói ${heroName} tem o XP de ${experience} Prata`
     }else if(experience >= 5001 && experience <= 7000){
-        document.getElementById("levelresult").innerText = `O herói ${heroName} tem o XP de ${experience} Ouro`
+        document.getElementById("levelresult").innerText = `Resultado: O herói ${heroName} tem o XP de ${experience} Ouro`
     }else if(experience >= 7001 && experience <= 8000){
-        document.getElementById("levelresult").innerText = `O herói ${heroName} tem o XP de ${experience} Platina`
+        document.getElementById("levelresult").innerText = `Resultado: O herói ${heroName} tem o XP de ${experience} Platina`
     }else if(experience >= 8001 && experience <= 9000){
-        document.getElementById("levelresult").innerText = `O herói ${heroName} tem o XP de ${experience} Ascendente`
+        document.getElementById("levelresult").innerText = `Resultado: O herói ${heroName} tem o XP de ${experience} Ascendente`
     }else if(experience >= 9001 && experience <= 10000){
-       document.getElementById("levelresult").innerText = `O herói ${heroName} tem o XP de ${experience} Imortal`
+       document.getElementById("levelresult").innerText = `Resultado: O herói ${heroName} tem o XP de ${experience} Imortal`
     }else 
-        document.getElementById("levelresult").innerText = `O herói ${heroName} tem o XP de ${experience} Radiante`
+        document.getElementById("levelresult").innerText = `Resultado:O herói ${heroName} tem o XP de ${experience} Radiante`
 }
 
 function clearForm(){
